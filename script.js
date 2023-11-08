4aad9cc13ba76a1b894ba688b49ca2421166c68c
shaikkhairunnisa <shaikkhairunnisa900@gmail.com>
2023-11-08 22:40:15 +0530

    weather app

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const city = 'Denver';
+    const temperature = '51°C';
+
+    
+    document.getElementById('city-name').textContent = city;
+    document.getElementById('temperature').textContent = temperature;
+});
